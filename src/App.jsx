e62c698d68ee72c17bda00dc6971d0434979ae77
Syed Aidah Rufai <syedaidahrufai@Syeds-MacBook-Air.local>
2026-04-10 23:04:04 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+function App() {
+  return (
+    <>
+    <Navbar />
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+    </>
+  
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <h1>IntelliSentry</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    )   
+}
+export default Navbar;
